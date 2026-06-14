5e4510c6c26efec69a0ef00b19b7667e6d13a71f
adeeliqbalanjum <bitm-f20-090@superior.edu.pk>
2026-06-14 18:38:02 +0500

    Restore portfolio homepage hero target

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,5 @@
 import DigitalistsPortfolio from '@/components/DigitalistsPortfolio';
-import Real3DGlobalLayer from '@/components/Real3DGlobalLayer';
 
 export default function Home() {
-  return (
-    <>
-      <DigitalistsPortfolio />
-      <Real3DGlobalLayer />
-    </>
-  );
+  return <DigitalistsPortfolio />;
 }
